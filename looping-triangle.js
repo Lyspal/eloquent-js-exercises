76033cd6fcb8551fe54dba12263fc7c12558084e
Sylvain Laporte <laporte.sylvain@outlook.com>
2018-11-25 23:39:34 -0500

    End of exercise Ch.2.1

---
+++ looping-triangle.js
@@ -0,0 +1,25 @@
+/* Outputs:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+*/
+
+// My implementation
+let numberOfFloors = 7;
+let floor = "";
+
+for (let counter = 1; counter <= numberOfFloors; counter++) {
+    floor += "#";
+    console.log(floor);
+}
+
+console.log("");
+
+// Solution
+for (let floor = "#"; floor.length < 8; floor += "#") {
+    console.log(floor);
+}
